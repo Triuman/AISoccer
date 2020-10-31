553e4e662014ff1e05fb633bdf9d6c126f175ec4
triuman <hasan.tekin@helpline.pmcs.de>
2020-10-31 17:17:17 +0100

    Inital Commit
    - Basic neural network feedforwarding is working.
    - Basic player/ball mechanics are working.

---
+++ Assets/InputActions/PlayerActions.cs
@@ -0,0 +1,231 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputActions/PlayerActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInputActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInputActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerActions"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""9ca2c426-c604-42d2-88b9-29b245b73de5"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""07b3ef06-c427-4272-8e64-a2cd616c318a"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Shoot"",
+                    ""type"": ""Button"",
+                    ""id"": ""9a00e63a-ebab-4f81-80d7-f6695aa13beb"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""22603456-5f38-4f29-83eb-7c254e0e79ce"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""0712893d-1cd6-4669-9b7e-3152dda018d0"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""81ff8bcb-789f-4dab-8039-8e46939020ce"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""41016d05-b33e-4189-81a0-d39f930692d1"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""8bab1d62-6f74-4a3a-92dc-46a3a1783b92"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""33127e72-fb09-4c2b-8965-e499b06a1c1a"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Shoot"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard&Mouse"",
+            ""bindingGroup"": ""Keyboard&Mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_Shoot = m_Player.FindAction("Shoot", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_Shoot;
+    public struct PlayerActions
+    {
+        private @PlayerInputActions m_Wrapper;
+        public PlayerActions(@PlayerInputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputAction @Shoot => m_Wrapper.m_Player_Shoot;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Shoot.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
+                @Shoot.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
+                @Shoot.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Shoot.started += instance.OnShoot;
+                @Shoot.performed += instance.OnShoot;
+                @Shoot.canceled += instance.OnShoot;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    private int m_KeyboardMouseSchemeIndex = -1;
+    public InputControlScheme KeyboardMouseScheme
+    {
+        get
+        {
+            if (m_KeyboardMouseSchemeIndex == -1) m_KeyboardMouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard&Mouse");
+            return asset.controlSchemes[m_KeyboardMouseSchemeIndex];
+        }
+    }
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnShoot(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/NeuralNetwork.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    public class NeuralNetwork
+    {
+        public NeuralNetwork(int[] layers)
+        {
+            Weights = new double[layers.Length - 1][,];
+            Biases = new double[layers.Length - 1][];
+
+            var rnd = new Random(Guid.NewGuid().GetHashCode());
+
+            for (int i = 0; i < layers.Length - 1; i++)
+            {
+                Weights[i] = new double[layers[i+1], layers[i]];
+                Biases[i] = new double[layers[i+1]];
+
+                for (int a = 0; a < layers[i+1]; a++)
+                {
+                    for (int b = 0; b < layers[i]; b++)
+                    {
+                        Weights[i][a, b] = rnd.NextDouble() * 2 - 1f;
+                    }
+                    Biases[i][a] = rnd.NextDouble() * 2 - 1f;
+                }
+            }
+        }
+
+        public double[][,] Weights { get; set; }
+        public double[][] Biases { get; set; }
+
+        public static double[] FeedForward(NeuralNetwork nn, double[] inputs)
+        {
+            return FeedForward(inputs, nn.Weights, nn.Biases, 0);
+        }
+        public static double[] FeedForward(double[] inputs, double [][,] weights, double [][] biases, int layer)
+        {
+            var output = Sigmoid(Matrix.add(Matrix.multiply(weights[layer], inputs), biases[layer]));
+            if (layer == biases.Length - 1)
+                return output;
+            return FeedForward(Sigmoid(Matrix.add(Matrix.multiply(weights[layer], inputs), biases[layer])), weights, biases, layer + 1);
+        }
+
+        public static double[] Sigmoid(double[] X)
+        {
+            var result = new double[X.Length];
+            for (int i = 0; i < X.Length; i++)
+            {
+                result[i] = 1.0 / (1.0 + Math.Exp(-X[i]));
+            }
+            return result;
+        }
+        public static double Sigmoid(double x)
+        {
+            return 1.0 / (1.0 + Math.Exp(-x));
+        }
+
+        public static double LogSigmoid(double x)
+        {
+            if (x < -45.0) return 0.0;
+            else if (x > 45.0) return 1.0;
+            else return 1.0 / (1.0 + Math.Exp(-x));
+        }
+
+        public static double HyperbolicTangtent(double x)
+        {
+            if (x < -45.0) return -1.0;
+            else if (x > 45.0) return 1.0;
+            else return Math.Tanh(x);
+        }
+    }
+
+
+    class Matrix
+    {
+
+        public static double[] add(double[] v1, double[] v2)
+        {
+            double[] result = new double[v1.Length];
+            for (int i = 0; i < v1.Length; i++)
+            {
+                result[i] = v1[i] + v2[i];
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="m">dim: axb</param>
+        /// <param name="v">dim: b</param>
+        /// <returns>dim: a</returns>
+        public static double[] multiply(double[,] m, double[] v)
+        {
+            // row x column
+            // m axb    v b 	result a
+            int a = m.GetLength(0);
+            int b = v.Length;
+            int c = 1;
+
+            var result = new double[a];
+            for (var cc = 0; cc < c; cc++)
+            {
+                for (var aa = 0; aa < a; aa++)
+                {
+                    for (var bb = 0; bb < b; bb++)
+                    {
+                        result[aa] += m[aa, bb] * v[bb];
+                    }
+                }
+            }
+
+            return result;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="m1">dim: axb</param>
+        /// <param name="m2">dim: bxc</param>
+        /// <returns>dim: axc</returns>
+        public static double[,] multiply(double[,] m1, double[,] m2)
+        {
+            // row x column
+            // m1 axb    m2 bxc 	result axc
+            int a = m1.GetLength(0);
+            int b = m1.GetLength(1);
+            int c = m2.GetLength(1);
+
+            var result = new double[a, c];
+            for (var cc = 0; cc < c; cc++)
+            {
+                for (var aa = 0; aa < a; aa++)
+                {
+                    for (var bb = 0; bb < b; bb++)
+                    {
+                        result[aa, cc] += m1[aa, bb] * m2[bb, cc];
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static double dotProduct(double[] v1, double[] v2)
+        {
+            double sum = 0;
+            for (var i = 0; i < v1.Length; i++)
+            {
+                sum += v1[i] * v2[i];
+            }
+            return sum;
+        }
+
+        public static void printMatrix(double[,] m)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    Console.Write(string.Format("{0} ", m[i, j]));
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
+        public static void printVector(double[] v)
+        {
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.WriteLine(string.Format("{0} ", v[i]));
+            }
+        }
+
+    }
+}
