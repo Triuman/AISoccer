8236da42322d17d43bfd539403cc8c916973837e
Triuman <hs@demodern.de>
2021-10-17 10:56:10 +0200

    Added Ray input but not using it due to performance issues

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,16 +5,28 @@ public class Ball : MonoBehaviour
 {
     public event EventHandler OnRightGoalCollisionEnter;
     public event EventHandler OnRightGoalCollisionStay;
+    public event EventHandler OnLeftGoalCollisionEnter;
+    public event EventHandler OnLeftGoalCollisionStay;
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.CompareTag("RightGoal"))
         {
             OnRightGoalCollisionEnter?.Invoke(this, null);
         }
+        else
+        if (collider.CompareTag("LeftGoal"))
+        {
+            OnLeftGoalCollisionEnter?.Invoke(this, null);
+        }
     }
     private void OnTriggerStay2D(Collider2D collider)
     {
         if (collider.CompareTag("RightGoal"))
+        {
+            OnRightGoalCollisionStay?.Invoke(this, null);
+        }
+        else
+        if (collider.CompareTag("LeftGoal"))
         {
             OnRightGoalCollisionStay?.Invoke(this, null);
         }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Diagnostics;
@@ -15,6 +16,8 @@ namespace Assets.Scripts
         public event EventHandler OnShootTheBall;
         public event EventHandler OnRightGoalCollisionEnter;
         public event EventHandler OnRightGoalCollisionStay;
+        public event EventHandler OnLeftGoalCollisionEnter;
+        public event EventHandler OnLeftGoalCollisionStay;
         public event EventHandler OnBallCollisionEnter;
         public event EventHandler OnBallCollisionStay2D;
         public event EventHandler OnCornerCollisionStay2D;
@@ -31,7 +34,7 @@ namespace Assets.Scripts
         new private Rigidbody2D rigidbody;
         new private Renderer renderer;
         private SpriteRenderer spriteRenderer;
-
+        private CircleCollider2D circleCollider2D;
 
         private const float BallHitDistance = 0.6f;
         private const float BallHitForce = 50f;
@@ -52,6 +55,7 @@ namespace Assets.Scripts
             ballCollider = ball.GetComponent<Collider2D>();
             ballRenderer = ball.GetComponent<Renderer>();
             ballSpriteRenderer = ball.GetComponent<SpriteRenderer>();
+            circleCollider2D = GetComponent<CircleCollider2D>();
 
             playerActions = new PlayerInputActions();
             playerActions.Player.Move.performed += Move_performed;
@@ -61,11 +65,16 @@ namespace Assets.Scripts
 
             ball.OnRightGoalCollisionEnter += Ball_OnRightGoalCollisionEnter;
             ball.OnRightGoalCollisionStay += Ball_OnRightGoalCollisionStay;
+            ball.OnLeftGoalCollisionEnter += Ball_OnLeftGoalCollisionEnter;
+            ball.OnLeftGoalCollisionStay += Ball_OnLeftGoalCollisionStay;
         }
 
-        private void OnDestroy() {
+        private void OnDestroy()
+        {
             ball.OnRightGoalCollisionEnter -= Ball_OnRightGoalCollisionEnter;
             ball.OnRightGoalCollisionStay -= Ball_OnRightGoalCollisionStay;
+            ball.OnLeftGoalCollisionEnter -= Ball_OnLeftGoalCollisionEnter;
+            ball.OnLeftGoalCollisionStay -= Ball_OnLeftGoalCollisionStay;
         }
 
         private void Ball_OnRightGoalCollisionEnter(object sender, System.EventArgs e)
@@ -78,6 +87,15 @@ namespace Assets.Scripts
             OnRightGoalCollisionStay?.Invoke(this, null);
         }
 
+        private void Ball_OnLeftGoalCollisionEnter(object sender, System.EventArgs e)
+        {
+            OnLeftGoalCollisionEnter?.Invoke(this, null);
+        }
+
+        private void Ball_OnLeftGoalCollisionStay(object sender, System.EventArgs e)
+        {
+            OnLeftGoalCollisionStay?.Invoke(this, null);
+        }
         // We want to reuse this object instead of creating new ones each generation.
         public void Reset(Vector2 playerPos)
         {
@@ -174,6 +192,76 @@ namespace Assets.Scripts
             // inputs[3] = diffVec.y;
 
             return inputs;
+        }
+
+
+        const int RayCount = 10;
+        const int ObjectCount = 6;
+        const int ObjectVectorLength = ObjectCount + 3;
+        // 6 x object type, distance, relative velocity
+        Dictionary<string, double[]> ObjectVectorDic = new Dictionary<string, double[]>{
+            {"Ball", new[] { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0 }},
+            {"InnerWall", new[] { 0.0, 1.0, 0.0, 0.0, 0.0, 0.0 }},
+            {"TeamGoal", new[] { 0.0, 0.0, 1.0, 0.0, 0.0, 0.0 }},
+            {"OpponentGoal", new[] { 0.0, 0.0, 0.0, 1.0, 0.0, 0.0 }},
+            {"Teammate", new[] { 0.0, 0.0, 0.0, 0.0, 1.0, 0.0 }},
+            {"Opponent", new[] { 0.0, 0.0, 0.0, 0.0, 0.0, 1.0 }}
+        };
+
+        // This method is using rays to see what is around the player, how far they are and how fast they are moving
+        public double[] PrepareInputsRays()
+        {
+            double[] inputs = new double[RayCount * ObjectVectorLength];
+            circleCollider2D.enabled = false;
+            transform.gameObject.layer = 6;
+            ball.gameObject.layer = 6;
+            var anglePerRay = Mathf.PI * 2 / RayCount;
+            Vector2 velocity = rigidbody.velocity;
+            for (int i = 0; i < RayCount; i++)
+            {
+                var angle = anglePerRay * i - Mathf.PI;
+                var direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+                var hit = Physics2D.Raycast(transform.position, direction);
+                if (!hit)
+                    return inputs;
+                var objectVec = GetObjectVector(hit.transform.tag);
+                for (int v = 0; v < ObjectCount; v++)
+                {
+                    inputs[i * ObjectVectorLength + v] = objectVec[v];
+                }
+                var distance = Vector2.Distance(transform.position, hit.transform.position);
+                if (hit.rigidbody)
+                    velocity -= hit.rigidbody.velocity;
+                inputs[i * ObjectVectorLength + ObjectCount] = distance;
+                inputs[i * ObjectVectorLength + ObjectCount + 1] = velocity.x;
+                inputs[i * ObjectVectorLength + ObjectCount + 2] = velocity.y;
+            }
+            circleCollider2D.enabled = true;
+            transform.gameObject.layer = 2;
+            ball.gameObject.layer = 2;
+
+            return inputs;
+        }
+
+        double[] GetObjectVector(string tag)
+        {
+            // TODO: decide which goal is opponent's
+            switch (tag)
+            {
+                case "Ball":
+                    return ObjectVectorDic["Ball"];
+                default:
+                case "InnerWall":
+                    return ObjectVectorDic["InnerWall"];
+                case "RightGoal":
+                    return ObjectVectorDic["TeamGoal"];
+                case "LeftGoal":
+                    return ObjectVectorDic["OpponentGoal"];
+                case "RedPlayer":
+                    return ObjectVectorDic["Teammate"];
+                case "BluePlayer":
+                    return ObjectVectorDic["Opponent"];
+            }
         }
 
         public void ApplyOutput(double[] output)
